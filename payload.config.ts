c34741f59d510a811f147e9cf3c5b2c8b9b8e8c3
Jaimay <jamarwarnerjohnson@gmail.com>
2026-08-06 18:17:38 -0400

    test: temporarily removing the blob storage

--- payload.config.ts
+++ payload.config.ts
@@ -36,15 +36,5 @@ export default buildConfig({
 	  },
   }),
   sharp,
-  plugins: [
-    vercelBlobStorage({
-      enabled: true,
-      collections: {
-        media: true,
-      },
-      token: process.env.BLOB_READ_WRITE_TOKEN,
-      clientUploads: true,
-    }),
-
-  ],
+  plugins: [],
 });
